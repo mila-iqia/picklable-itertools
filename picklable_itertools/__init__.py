13ad85d9bfd699c294294a69794efe3743e7fdd8
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2015-02-11 14:05:39 -0500

    Add file_iterator

--- picklable_itertools/__init__.py
+++ picklable_itertools/__init__.py
@@ -1,8 +1,9 @@
 from abc import ABCMeta, abstractmethod
-import six
 import collections
+import io
+import six
 
-from picklable_itertools.version import __version__
+from picklable_itertools.version import __version__  # noqa
 
 
 @six.add_metaclass(ABCMeta)
@@ -19,10 +20,38 @@ def _iter(obj):
     """A custom replacement for iter(), dispatching a few custom picklable
     iterators for known types.
     """
+    if six.PY2:
+        file_types = file,
+    if six.PY3:
+        file_types = io.IOBase,
+
     if six.PY2 and isinstance(obj, (list, tuple)):
         return ordered_sequence_iterator(obj)
+    if isinstance(obj, file_types):
+        return file_iterator(obj)
     else:
         return iter(obj)
+
+
+class file_iterator(BaseItertool):
+    """A picklable file iterator."""
+    def __init__(self, f):
+        self._f = f
+
+    def __next__(self):
+        line = self._f.readline()
+        if not line:
+            raise StopIteration
+        return line
+
+    def __getstate__(self):
+        name, pos, mode = self._f.name, self._f.tell(), self._f.mode
+        return name, pos, mode
+
+    def __setstate__(self, state):
+        name, pos, mode = state
+        self._f = open(name, mode=mode)
+        self._f.seek(pos)
 
 
 class ordered_sequence_iterator(BaseItertool):
